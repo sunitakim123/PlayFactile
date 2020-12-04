17bd9e737ef1c354c6ed06fb022a2a8823eb55f7
sunita <sunitakim123@gmail.com>
2020-12-04 16:30:09 +0530

    dec 4 25thcom

--- src/test/java/Factile_Mode/DGBP_HSB_TestCase_22.java
+++ src/test/java/Factile_Mode/DGBP_HSB_TestCase_22.java
@@ -234,15 +234,18 @@ public class DGBP_HSB_TestCase_22 extends Base {
 					driver1.switchTo().window(driver1.getWindowHandle());
 
 					wait1.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//span[@class='mr-2']")));
-					wait1.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[contains(text(),'Buzz!')]")));
+					//wait1.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[contains(text(),'Buzz!')]")));
 					//wait1.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[contains(text(),'Buzz!')]")));
 			
-					WebElement element =driver1.findElement(By.xpath("//div[contains(text(),'Buzz!')]"));
-					element.click();
-				//	Actions actions = new Actions(driver1);
+				//	WebElement element =driver1.findElement(By.xpath("//div[contains(text(),'Buzz!')]"));
+					//element.click();
+				//Actions actions = new Actions(driver1);
 				//	actions.moveToElement(element).click().build().perform();
 					//JavascriptExecutor jse = (JavascriptExecutor)driver1;
 					//jse.executeScript("arguments[0].click()", element);
+					
+					wait1.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[contains(text(),'Buzz!')]")));
+					driver1.findElement(By.xpath("//div[contains(text(),'Buzz!')]")).click();
 					wait1.until(ExpectedConditions
 							.presenceOfElementLocated(By.xpath("//input[@placeholder='Enter Answer']")));
 					driver1.findElement(By.xpath("//input[@placeholder='Enter Answer']")).sendKeys("test");
@@ -251,13 +254,12 @@ public class DGBP_HSB_TestCase_22 extends Base {
 					driver1.findElement(By.xpath("//input[@class='ansSubmitBtn btn--inside uppercase']")).click();
 					// driver.switchTo().window(driver.getWindowHandle());
 					driver.switchTo().window(driver.getWindowHandle());
-					Thread.sleep(8000);
-					driver.manage().timeouts().implicitlyWait(120, TimeUnit.SECONDS);
+					Thread.sleep(2000);
 					wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//i[@class='fa fa-check right']")));
 					driver.findElement(By.xpath("//i[@class='fa fa-check right']")).click();
 					wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[@class='backToBoard']")));
 					driver.findElement(By.xpath("//button[@class='backToBoard']")).click();
-				
+					Thread.sleep(500);
 				Thread.sleep(2000);
 				}
 					wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//h2[@class='winnerTopHeader']")));
