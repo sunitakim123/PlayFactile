c6744b49e02acc6394ce99857cda6d38fe948d5a
sunita <sunitakim123@gmail.com>
2020-12-04 17:32:09 +0530

    dec 4 28th..

--- src/test/java/Factile_Mode/DGBP_HSB_TestCase_22.java
+++ src/test/java/Factile_Mode/DGBP_HSB_TestCase_22.java
@@ -99,9 +99,9 @@ public class DGBP_HSB_TestCase_22 extends Base {
 	@AfterTest
 	public void tearDown() throws InterruptedException {
 
-		driver1.quit();
-		driver.switchTo().window(driver.getWindowHandle());
-		driver.quit();
+	//	driver1.quit();
+		//driver.switchTo().window(driver.getWindowHandle());
+	//	driver.quit();
 	}
 
 	public void modeaterscreen() throws InterruptedException {
@@ -234,6 +234,7 @@ public class DGBP_HSB_TestCase_22 extends Base {
 					driver1.switchTo().window(driver1.getWindowHandle());
 
 					wait1.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//span[@class='mr-2']")));
+					Thread.sleep(5000);
 					//wait1.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[contains(text(),'Buzz!')]")));
 					//wait1.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[contains(text(),'Buzz!')]")));
 			
